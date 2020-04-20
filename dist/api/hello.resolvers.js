0ae0857b375adf2323d8aec6b462f76e3fbc87f8
brettbullock <brett.bullock@shopify.com>
2020-04-20 05:26:34 -0700

    remove build folder

--- dist/api/hello.resolvers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const resolvers = {
-    Query: {
-        hello: () => "world"
-    }
-};
-exports.default = resolvers;
-//# sourceMappingURL=hello.resolvers.js.map
