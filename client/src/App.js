c59631771bd450725a2ffb57937ec1996fb6c5e0
brettbullock <brett.bullock@shopify.com>
2020-04-23 05:21:18 -0700

    react apps created

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>hello</div>
+      <button onClick={() => console.log("Ive been clicked")}>Click me!</button>
+    </>
+  );
+}
+
+export default App;
